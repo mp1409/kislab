09024c736fbd77c356b35b2023a13c60bb24a76f
Michael Pfeiffer <michael.pfeiffer@eichenstein.de>
2016-05-03 19:19:11 +0200

    Added scripts to clean builddir, build doc and check source code.

---
+++ code/check-code.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/sh
+cppcheck -q -v --std=c++11 \
+	--enable=warning,style,performance,portability,unusedFunction \
+	src
---
+++ code/clean.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+scons -c
+
+rm -rf doc/html/*
+rm -rf doc/latex/*
