55b4ae4a7637d1a1ef3af90c30a9d0e24a0645bb
Michael Pfeiffer <michael.pfeiffer@eichenstein.de>
2016-05-13 01:51:21 +0200

    Added comments to main file.

--- code/kislab/kislab.ino
+++ code/kislab/kislab.ino
@@ -1,5 +1,18 @@
+/**
+ * @file
+ *
+ * Main file of the programm, containing setup and loop functions.
+ */
+
+/**
+ * The setup function, invoked once when the Arduino is powered on or the reset
+ * button was pressed.
+ */
 void setup() {
 }
 
+/**
+ * The main loop function, run continiously.
+ */
 void loop() {
 }
