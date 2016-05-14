e5052f08fe204b31be08f610e50b365ce3408d12
Michael Pfeiffer <michael.pfeiffer@eichenstein.de>
2016-05-14 18:43:07 +0200

    Moved installation of julia packages to custom script. I really do hope that julia-2ac304dfba is a stable directory name...

--- laborbericht/install_julia.sh
+++ laborbericht/install_julia.sh
@@ -5,9 +5,11 @@ bit=`uname -m`
 
 if [ "$bit" = "x86_64" ]; then
     wget https://julialang.s3.amazonaws.com/bin/linux/x64/0.4/julia-0.4.5-linux-x86_64.tar.gz
-else 
+else
     wget https://julialang.s3.amazonaws.com/bin/linux/x86/0.4/julia-0.4.5-linux-i686.tar.gz
 fi
 
 tar -xzf julia-0.4.5-linux*.tar.gz* -C julia/
 rm julia-0.4.5-linux*.tar.gz*
+
+./install_julia_pkgs.sh
---
+++ laborbericht/install_julia_pkgs.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+string="Pkg.update(); Pkg.add(\"Gadfly\"); Pkg.add(\"Cairo\")"
+
+# Test if julia is in path
+julia --version
+retval=$?
+
+if [ $retval -eq 127 ]; then
+	echo $string | ./julia/julia-2ac304dfba/bin/julia
+else
+	echo $string | julia
+fi
