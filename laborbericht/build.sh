d5d1a8d1360770e5c343bba97d6b4ab582ad31d8
Simon Buttgereit <simon.buttgereit@gmail.com>
2016-05-15 14:28:21 +0200

    juliascript um CSV datein zu plotten

--- laborbericht/build.sh
+++ laborbericht/build.sh
@@ -6,8 +6,10 @@ retval=$?
 
 if [ $retval -eq 127 ]; then
 	./julia/julia-2ac304dfba/bin/julia julia/*.j
+	./julia/julia-2ac304dfba/bin/julia julia/csv.jx julia/*.csv
 else
 	julia julia/*.j
+	julia julia/csv.jx julia/*.csv
 fi
 
 ./build_withoutplots.sh
