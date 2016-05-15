eb52e2f342be99a088303dc8f9b17e80692a0b41
Michael Pfeiffer <michael.pfeiffer@eichenstein.de>
2016-05-15 19:02:32 +0200

    Perhaps bash will fix it.

--- code/travis-run.sh
+++ code/travis-run.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 cd $(dirname $(readlink -f "$0"))
 ./build_platform uno
--- code/travis-setup.sh
+++ code/travis-setup.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 cd $(dirname $(readlink -f "$0"))
 source <(curl -SLs https://raw.githubusercontent.com/adafruit/travis-ci-arduino/master/install.sh)
