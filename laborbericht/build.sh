8347c37fb5f41adad663e877bc6988a1af17a95e
Michael Pfeiffer <michael.pfeiffer@eichenstein.de>
2016-05-15 15:05:56 +0200

    According to the Internet, julia files do now have a *.jl extension.

--- laborbericht/build.sh
+++ laborbericht/build.sh
@@ -5,11 +5,11 @@ julia --version
 retval=$?
 
 if [ $retval -eq 127 ]; then
-	./julia/julia-2ac304dfba/bin/julia julia/*.j
-	./julia/julia-2ac304dfba/bin/julia julia/csv.jx julia/*.csv
+	./julia/julia-2ac304dfba/bin/julia julia/*.jl
+	./julia/julia-2ac304dfba/bin/julia julia/csv.jl julia/*.csv
 else
-	julia julia/*.j
-	julia julia/csv.jx julia/*.csv
+	julia julia/*.jl
+	julia julia/csv.jl julia/*.csv
 fi
 
 ./build_withoutplots.sh
