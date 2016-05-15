f3073c456d41c435c3b2d4bfa12d25ff1ee68e10
Michael Pfeiffer <michael.pfeiffer@eichenstein.de>
2016-05-15 18:06:57 +0200

    Try to trick arduino to run on a headless build node.

---
+++ code/build-headless.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+/sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_1.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :1 -ac -screen 0 1280x1024x16
+sleep 3
+export DISPLAY=:1.0
+
+./build.sh
