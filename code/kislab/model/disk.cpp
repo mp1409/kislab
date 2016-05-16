7fb151c3d4a391fafbdd2edd416a40a6b8499bc4
Michael Pfeiffer <michael.pfeiffer@eichenstein.de>
2016-05-16 16:14:38 +0200

    Make TODOS accessible by doxygen.

--- code/kislab/model/disk.cpp
+++ code/kislab/model/disk.cpp
@@ -20,12 +20,13 @@ Disk::~Disk() {
 }
 
 void Disk::go() {
-	// sensoren auswerten und logs printen die in diagramme gepackt werden können
+	/**
+	 * \todo geschwindikeit messen
+	 * \todo anständige logik für wann fallen lassen implementieren
+	 * \todo sensoren auswerten und logs printen die in diagramme gepackt
+	 * werden können
+	 */
 	if(pSensor->read() == 1 and hSensor->read() == 1) {
 		release->open();
 	}
-	/* TODO:
-	*  - geschwindikeit messen
-	*  - anständige logik für wann fallen lassen implementieren
-	*/
 }
