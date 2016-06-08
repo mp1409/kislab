5042b497666fd1cae6e2d2009bdec0e2d29654f7
Simon Buttgereit <simon.buttgereit@gmail.com>
2016-06-08 16:20:44 +0200

    approximation with 3 lin functions works pretty well

--- laborbericht/julia/plot-data.jl
+++ laborbericht/julia/plot-data.jl
@@ -27,11 +27,29 @@ end
 
 rot = map(x -> (x * 5) / (interval / 1000), slopes)
 
-f = curve_fit(LinearFit, map(x -> Float64(x * (interval / 1000)), collect(1:size(rot)[1])), convert(Array{Float64, 1}, rot))
+# f = curve_fit(LinearFit, map(x -> Float64(x * (interval / 1000)), collect(1:size(rot)[1])), convert(Array{Float64, 1}, rot))
+f1 = curve_fit(
+        LinearFit, 
+        map(x -> Float64(x * (interval / 1000)), collect(1:div(size(rot)[1], 3))), 
+        convert(Array{Float64, 1}, rot[1:div(end, 3)])
+    )
+f2 = curve_fit(
+        LinearFit, 
+        map(x -> Float64(x * (interval / 1000)), collect(div(size(rot)[1], 3):2 * div(size(rot)[1], 3))), 
+        convert(Array{Float64, 1}, rot[div(end, 3):2 * div(end, 3)])
+    )
+f3 = curve_fit(
+        LinearFit, 
+        map(x -> Float64(x * (interval / 1000)), collect(2 * div(size(rot)[1], 3):size(rot)[1])), 
+        convert(Array{Float64, 1}, rot[2 * div(end, 3):end])
+    )
 
 
 println(string(m[:name], ": Berechnungen erfolgreich beendet."))
-println(string("Approx:\t", f))
+println("Approx:")
+println(string("\t", f1))
+println(string("\t", f2))
+println(string("\t", f3))
 print("Plotte Graph...")
 println()
 
@@ -41,20 +59,32 @@ p = plot(
         x = map(x -> Int(x * (interval / 1000)), collect(1:size(rot)[1])),
         y = rot[1:end],
         Geom.point,
-        Geom.smooth
+        # Geom.smooth
+    ),
+    # layer(
+    #     x =  map(x -> Float64(x * (interval / 1000)), collect(1:size(rot)[1] - 2)),
+    #     y = f(map(x -> Float64(x * (interval / 1000)), collect(1:size(rot)[1] - 2))),
+    #     Geom.line,
+    #     Theme(default_color=colorant"orange")
+    # ),
+    layer(
+        x =  map(x -> Float64(x * (interval / 1000)), collect(1:div(size(rot)[1], 3))),
+        y = f1(map(x -> Float64(x * (interval / 1000)), collect(1:div(size(rot)[1], 3)))),
+        Geom.line,
+        # Theme(default_color=colorant"red")
     ),
     layer(
-        x =  map(x -> Float64(x * (interval / 1000)), collect(1:size(rot)[1] - 2)),
-        y = f(map(x -> Float64(x * (interval / 1000)), collect(1:size(rot)[1] - 2))),
+        x =  map(x -> Float64(x * (interval / 1000)), collect(div(size(rot)[1], 3):2 * div(size(rot)[1], 3))),
+        y = f2(map(x -> Float64(x * (interval / 1000)), collect(div(size(rot)[1], 3):2 * div(size(rot)[1], 3)))),
         Geom.line,
-        Theme(default_color=colorant"orange")
+        # Theme(default_color=colorant"red")
     ),
-    #layer(
-    #    x = a[2:end, 1],
-    #    y = a[2:end, 3],
-    #    Geom.step,
-    #    Geom.smooth,
-    #),
+    layer(
+        x =  map(x -> Float64(x * (interval / 1000)), collect(div(2 * size(rot)[1], 3):size(rot)[1])),
+        y = f3(map(x -> Float64(x * (interval / 1000)), collect(div(2 * size(rot)[1], 3):size(rot)[1]))),
+        Geom.line,
+        # Theme(default_color=colorant"red")
+    ),
     Guide.XLabel("Zeit [s]"),
     Guide.YLabel("U/min"),
     #Guide.manual_color_key(
