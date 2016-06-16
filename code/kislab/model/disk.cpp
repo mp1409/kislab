8aaf752b5ee899d794fe4182c39783b2a607fbb9
Michael Pfeiffer <michael.pfeiffer@eichenstein.de>
2016-06-16 22:04:54 +0200

    We need to record the time already at the first readings of the sensors.

--- code/kislab/model/disk.cpp
+++ code/kislab/model/disk.cpp
@@ -8,6 +8,7 @@
 
 void Disk::update() {
 	if(_pSensorLastValue == Sensor::Value::INVALID) {
+		_pSensorLastTimes[_timeIndex] = millis();
 		_pSensorLastValue = _pSensor->read();
 	} else if (_pSensorLastValue != _pSensor->read()) {
 		_timeIndex = (_timeIndex + 1) % _pSensorSampleSize;
@@ -16,6 +17,7 @@ void Disk::update() {
 	}
 
 	if(_position.value == Sensor::Value::INVALID) {
+		_position.time = millis();
 		_position.value = _hSensor->read();
 	} else if(_position.value != _hSensor->read()) {
 		_position.time = millis();
