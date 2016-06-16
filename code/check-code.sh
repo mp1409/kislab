1030aacb8dfd604aecc44156a4a3fe2a4a32afdd
Michael Pfeiffer <michael.pfeiffer@eichenstein.de>
2016-06-16 22:25:01 +0200

    Do not let cppcheck inspect header files seperately.

--- code/check-code.sh
+++ code/check-code.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-find kislab | grep -E '*.(h|cc|ino)$' | xargs cppcheck -q -v --std=c++11 \
+find kislab | grep -E '*.(cc|ino)$' | xargs cppcheck -q -v --std=c++11 \
 	--error-exitcode=1 --enable=warning,style,performance,portability
