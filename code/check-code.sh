ad085101c889e27990fdf4606b21834ff16b792d
Michael Pfeiffer <michael.pfeiffer@eichenstein.de>
2016-06-30 11:01:27 +0200

    Made build scripts compatible with apple stuff.

--- code/check-code.sh
+++ code/check-code.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-find kislab | grep -E '*.(cpp|ino)$' | xargs cppcheck -q -v --std=c++11 \
+find kislab | grep -E '.*[.](cpp|ino)$' | xargs cppcheck -q -v --std=c++11 \
 	--error-exitcode=1 --enable=warning,style,performance,portability
