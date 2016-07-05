b0d599c9a3405c080c3fbbbb7fda24d6a3edaf14
Simon Buttgereit <simon.buttgereit@gmail.com>
2016-07-05 10:22:04 +0200

    UML changes and pdf-output for building

--- laborbericht/build_withoutplots.sh
+++ laborbericht/build_withoutplots.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env sh
 
+which rsvg-convert
+retval=$?
+
+if [ $retval -eq 0 ]; then
+    for i in UML/*.svg 
+    do
+        name=`basename $i .svg`
+        rsvg-convert -f pdf -o UML/${name}.pdf $i
+    done
+fi
+
 pdflatex Laborbericht.tex
 pdflatex Laborbericht.tex
 pdflatex Laborbericht.tex
