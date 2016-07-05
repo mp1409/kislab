132c3185b9b84494f0fd8af7e9a8de20e17d94b9
Michael Pfeiffer <michael.pfeiffer@eichenstein.de>
2016-07-05 11:32:11 +0200

    Do not check whether librsvg is installed because we need it anyway.

--- laborbericht/build_withoutplots.sh
+++ laborbericht/build_withoutplots.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/env sh
 
-which rsvg-convert
-retval=$?
-
-if [ $retval -eq 0 ]; then
-    for i in UML/*.svg 
-    do
-        name=`basename $i .svg`
-        rsvg-convert -f pdf -o UML/${name}.pdf $i
-    done
-fi
+for i in UML/*.svg
+do
+	name=`basename $i .svg`
+	rsvg-convert -f pdf -o UML/${name}.pdf $i
+done
 
 pdflatex Laborbericht.tex
 pdflatex Laborbericht.tex
